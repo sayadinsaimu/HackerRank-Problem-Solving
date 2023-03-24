dd9ecfb396095e04576d7a3b2325a88cad440d95
Sayadin Saimu <55958502+sayadinsaimu@users.noreply.github.com>
2023-03-25 01:56:03 +0800

    OK

---
+++ A_Very_BigSum/Very_BigSum.java
@@ -0,0 +1,43 @@
+package A_Very_BigSum;
+
+import java.io.*;
+import java.util.*;
+
+class Result {
+
+	public static long aVeryBigSum(List<Long> ar) {
+		// Write your code here
+		long sum = 0;
+		for (int i = 0; i < ar.size(); i++) {
+			sum = sum + ar.get(i);
+		}
+		return sum;
+	}
+
+}
+
+public class Very_BigSum {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+		String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+		List<Long> ar = new ArrayList<>();
+
+		for (int i = 0; i < arCount; i++) {
+			long arItem = Long.parseLong(arTemp[i]);
+			ar.add(arItem);
+		}
+
+		long result = Result.aVeryBigSum(ar);
+
+		bufferedWriter.write(String.valueOf(result));
+		bufferedWriter.newLine();
+
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
+}
