9c81445203b5cda16cba81307fe882795dfdddc9
sayadin-saimu <sayadinsaimu5576@gmail.com>
2023-03-27 17:21:43 +0800

    OK

---
+++ Cats_and_Mouse/Cats_and_Mouse.java
@@ -0,0 +1,39 @@
+package Cats_and_Mouse;
+
+import java.io.*;
+import java.util.*;
+
+public class Cats_and_Mouse {
+	// Complete the catAndMouse function below
+	static String catAndMouse(int x, int y, int z) {
+		if(Math.abs(x-z)<Math.abs(y-z)) {
+			return "Cat A";
+		}
+		if(Math.abs(x-z)>Math.abs(y-z)) {
+			return "Cat B";
+		}
+		return "Mouse C";
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int q = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int qItr = 0; qItr < q; qItr++) {
+			String[] xyz = scanner.nextLine().split(" ");
+			int x = Integer.parseInt(xyz[0]);
+			int y = Integer.parseInt(xyz[1]);
+			int z = Integer.parseInt(xyz[2]);
+			String result = catAndMouse(x, y, z);
+
+			bufferedWriter.write(result);
+			bufferedWriter.newLine();
+		}
+		bufferedWriter.close();
+		scanner.close();
+	}
+}
